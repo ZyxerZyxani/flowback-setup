cfd3f7556c31d08eb8b78969a34be01456c0ae32
ZyxerZyxani <33463214+ZyxerZyxani@users.noreply.github.com>
2023-04-15 12:53:39 +0200

    Update npm.bash

--- resources/npm.bash
+++ resources/npm.bash
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-curl -sL https://deb.nodesource.com/setup_16.x | sudo -E bash -
-sudo apt install nodejs
-sudo apt install build-essential
-sudo npm install --global yarn
+package = "nodejs"
+
+# Check if package is already installed
+if dpkg-query -Wf'${db:Status-abbrev}' "$package" 2>/dev/null | grep -q '^i'; then
+    printf 'The package "%s" _is_ installed!\nSkipping.\n' "$package"
+    sudo npm install --global yarn
+else
+    curl -sL https://deb.nodesource.com/setup_16.x | sudo -E bash -
+    sudo apt install nodejs
+    sudo npm install --global yarn
+fi
