b2d696d7de0bd039c6e34742f4a3b04881d20ef3
ZyxerZyxani <33463214+ZyxerZyxani@users.noreply.github.com>
2023-04-18 22:23:14 +0200

    Update setup.bash

--- setup.bash
+++ setup.bash
@@ -11,3 +11,5 @@ cd /opt/flowback
 find $install_files| xargs -I % sh -c '%'
 cp $install_file/../../update_backend.bash backend/
 cp $install_file/../../update_frontend.bash frontend/
+cp $install_file/../templates/flowback/backend/.flowback_pgpass backend/
+printf 'Setup almost finnished. Modify /opt/flowback/backend/.flowback_pgpass and modify /opt/flowback/backend/update_backend.bash and /opt/flowback/frontend/update_frontend.bash for customisation.'
