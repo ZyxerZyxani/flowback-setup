07330ff5dbcfc68b05aa214c8943b9f33bb727a4
ZyxerZyxani <33463214+ZyxerZyxani@users.noreply.github.com>
2023-04-18 19:31:07 +0200

    Update setup.bash

--- setup.bash
+++ setup.bash
@@ -3,9 +3,11 @@
 sudo apt install git curl build-essential
 cd resources
 ls | xargs -I % sh -c 'sudo chmod +x % && ./%'
-cd resources
+cd flowback
 ls | xargs -I % sh -c 'sudo chmod +x %'
 install_files = "$PWD"
 mkdir -p /opt/flowback
 cd /opt/flowback
 find $install_files| xargs -I % sh -c '%'
+cp $install_file/../../update_backend.bash backend/
+cp $install_file/../../update_frontend.bash frontend/
