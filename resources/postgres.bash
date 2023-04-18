56c7b0b4a5b09413ee93f8517ae9b9db576e528e
ZyxerZyxani <33463214+ZyxerZyxani@users.noreply.github.com>
2023-04-18 18:47:42 +0200

    Update postgres.bash

--- resources/postgres.bash
+++ resources/postgres.bash
@@ -2,5 +2,23 @@
 
 # Install Postgres
 sudo apt update
-sudo apt install postgresql postgresql-contrib
+package = "postgres"
+
+# Check if package is already installed
+if dpkg-query -Wf'${db:Status-abbrev}' "$package" 2>/dev/null | grep -q '^i'; then
+    printf 'The package "%s" _is_ installed!\nSkipping.\n' "$package"
+else
+    sudo apt install postgresql
+fi
+
+package = "postgres-contrib"
+
+# Check if package is already installed
+if dpkg-query -Wf'${db:Status-abbrev}' "$package" 2>/dev/null | grep -q '^i'; then
+    printf 'The package "%s" _is_ installed!\nSkipping.\n' "$package"
+else
+    sudo apt install postgresql-contrib
+fi
+
+sudo systemctl enable postgresql.service
 sudo systemctl start postgresql.service
